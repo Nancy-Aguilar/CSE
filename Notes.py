fa87bda0f15e03073c0765ecba65260e8bac0e74
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2017-11-29 10:48:00 -0800

    Added functions
    11/29/17

--- Notes.py
+++ Notes.py
@@ -1,30 +1,84 @@
-print("Hello World")
+# print("Hello World")
+#
+# # Nancy
+# # (This is python 2.7)
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print # this makes a new line. In puthon 3.6, it looks like this: print()
+# print("See if you can figure this out")
+# print(5 % 3)
+#
+# car_name = "Nancy Mobile"
+# car_type = "Lamborghini Sesto Elemento"
+# car_cylinders = 8
+# car_mpg = 9000.1
+#
+# # Inline printing
+# print("I have a car called the %s" % car_name)
+# print("I have a car called the %s. It's a %s." % (car_name, car_type))
+#
+# #Asking for input
+# name = input("What is your name? ") #In python 3, it is just called input()
+# print("Hello %s." % name)
+#
+# age = input("How old are you? ")
+# print("%s Wow that's really old." % age)
 
-# Nancy
-# (This is python 2.7)
+# Functions
 
-print(3 + 5)
-print(5 - 3)
-print(3 * 5)
-print(6 / 2)
-print(3 ** 2)
 
-print # this makes a new line. In puthon 3.6, it looks like this: print()
-print("See if you can figure this out")
-print(5 % 3)
+def print_hw():
+    print("Hello World")
 
-car_name = "Nancy Mobile"
-car_type = "Lamborghini Sesto Elemento"
-car_cylinders = 8
-car_mpg = 9000.1
 
-# Inline printing
-print("I have a car called the %s" % car_name)
-print("I have a car called the %s. It's a %s." % (car_name, car_type))
+print_hw()
+print_hw()
+print_hw()
 
-#Asking for input
-name = input("What is your name? ") #In python 3, it is just called input()
-print("Hello %s." % name)
-age = input("How old are you? ")
-print("%s Wow that's really old." % age)
+
+def say_hi(name):  # name is a parameter
+    print("Hello %s." % name)
+    print("Enjoy your day.")
+
+
+say_hi("Jonah")
+
+
+def print_age(name, age):
+    print("%s is %d years old." % (name, age))
+    age += 1  # age = age + 1
+    print("Next year, they will be %d." % age)
+
+
+print_age("Jonah", 14)
+
+
+def f(x):
+    return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+# If statements
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    elif percentage >= 50:
+        return "F"
+
 
