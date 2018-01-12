b349d4776504dc5b306dd13744cc98b54e51c88a
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-12 11:15:50 -0800

    1/12/17
    the game is ready just need to display number of rounds played

--- Nancy-Lucky7's.py
+++ Nancy-Lucky7's.py
@@ -1,6 +1,4 @@
 import random
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
 
 print("Welcome to lucky 7's!")
 print("You start off with 15 dollars and each round is 1 dollar")
@@ -9,24 +7,25 @@ print("If you don't roll a 7 you lose the dollar you used to play")
 print("Have fun!")
 
 money = 15
-total = dice1 + dice2
-
 
 while money > 0:
     money -= 1
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    total = dice1 + dice2
+    rds = total
     print("Your current amount is now %s" % money)
     print("You rolled a %s" % dice1)
     print("You rolled a %s" % dice2)
     print("It's %s" % total)
+    if total == 7:
+        money += 5
+        print("You win")
+        print("Your current amount is now %s" % money)
+    if total > 7:
+        print("You lose")
+    if total < 7:
+        print("You lose")
 
-
-if total > 7:
-    print("You lose")
-
-if total < 7:
-    print("You lose")
-
-if total == 7:
-    print("You win")
-    money += 5
-    print("Your current amount is now %s" % money)
+if money == 0:
+    print("You played %s number of rounds")
