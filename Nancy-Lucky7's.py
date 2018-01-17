a3b9f6fff1c1a6b4167a4b4f4218ff537faaac92
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-17 10:44:47 -0800

    1/17/18
    finished lucky 7's

--- Nancy-Lucky7's.py
+++ Nancy-Lucky7's.py
@@ -7,25 +7,27 @@ print("If you don't roll a 7 you lose the dollar you used to play")
 print("Have fun!")
 
 money = 15
-
+rounds = 0
 while money > 0:
     money -= 1
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     total = dice1 + dice2
-    rounds = total
     print("Your current amount is now %s" % money)
     print("You rolled a %s" % dice1)
     print("You rolled a %s" % dice2)
-    print("It's %s" % total)
+    print("Your total is %s" % total)
     if total == 7:
+        rounds += 1
         money += 5
-        print("You win")
+        print("You won")
         print("Your current amount is now %s" % money)
     if total > 7:
-        print("You lose")
+        rounds += 1
+        print("You lost")
     if total < 7:
-        print("You lose")
+        rounds += 1
+        print("You lost")
     if money == 0:
         print("Game over, you have no money left to continue playing")
-        print("You played # rounds")
+        print("You played %s rounds" % rounds)
