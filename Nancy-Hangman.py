5b3936e86106c8d7999525b8a87144bab64fda04
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-22 10:46:51 -0800

    1/22/17
    have list for word

--- Nancy-Hangman.py
+++ Nancy-Hangman.py
@@ -1,2 +1,14 @@
+import random
 word_bank = ["Diamond", "Believe in yourself", "Pusheen the cat", "Space unicorn", "Chocolate", "Tiger", "Friendship",
-             "No precious jewel can be compared to you", "A blue rose", "Double Rainbow"]
+             "No precious jewel can be compared to you", "A blue rose", "Double rainbow"]
+
+word = random.choice(word_bank)
+
+str1 = word
+listOne = list(str1)
+print(listOne)
+
+guess = input("What is your guess?")
+
+if guess == word:
+    print("You win!")
