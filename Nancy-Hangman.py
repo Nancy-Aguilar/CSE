3373fcd9852c7f8157cc8bddc4c09291d1aca2b6
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-25 10:46:32 -0800

    1/25/17
    hid the letter but looks wierd

--- Nancy-Hangman.py
+++ Nancy-Hangman.py
@@ -3,16 +3,22 @@ word_bank = ["Diamond", "Believe in yourself", "Pusheen the cat", "Space unicorn
              "No precious jewel can be compared to you", "A blue rose", "Double rainbow"]
 
 word = random.choice(word_bank)
-
 str1 = word
 listOne = list(str1)
-print(listOne)
-
 guesses_left = 10
 letters_guessed = []
+
+for letter in listOne:
+    print("*")
 
 while guesses_left > 0:
     guesses_left -= 1
     guess = input("What is your guess?")
     letters_guessed.append(guess)
 
+    if guess == listOne[0]:
+        print("Yay")
+
+    if guess == word:
+        print("You win!")
+        quit()
