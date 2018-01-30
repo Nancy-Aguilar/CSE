be033ac7a0940aa8d9b999b81cadc096c73e748f
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-30 10:46:26 -0800

    1/30/18
    still need help

--- Nancy-Hangman.py
+++ Nancy-Hangman.py
@@ -1,18 +1,17 @@
 import random
 word_bank = ["Diamond", "Believe in yourself", "Pusheen the cat", "Space unicorn", "Chocolate", "Tiger", "Friendship",
-             "No precious jewel can be compared to you", "A blue rose", "Double rainbow"]
+             "Puglie the pug", "A blue rose", "Double rainbow"]
 
 word = random.choice(word_bank)
 str1 = word
 word_list = list(str1)
 guesses_left = 10
 letters_guessed = []
-
+length = len(word_list)
 for letter in word_list:
     print("*")
 
-while guesses_left > 0:
-    guesses_left -= 1
+while guesses_left:
     guess = input("What is your guess?")
     letters_guessed.append(guess)
 
