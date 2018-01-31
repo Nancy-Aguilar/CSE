0cad7184f699e2b82228dc896235769f79a3a592
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-01-31 10:46:45 -0800

    1/31/18
    done some work

--- Nancy-Hangman.py
+++ Nancy-Hangman.py
@@ -1,19 +1,21 @@
 import random
 word_bank = ["Diamond", "Believe in yourself", "Pusheen the cat", "Space unicorn", "Chocolate", "Tiger", "Friendship",
              "Puglie the pug", "A blue rose", "Double rainbow"]
-
 word = random.choice(word_bank)
 str1 = word
 word_list = list(str1)
 guesses_left = 10
 letters_guessed = []
-length = len(word_list)
-for letter in word_list:
-    print("*")
 
 while guesses_left:
+    for letter in word_list:
+        print("*")
     guess = input("What is your guess?")
     letters_guessed.append(guess)
 
-    if guess == word_list[0]:
+    if guess in word_list:
         print("Yay")
+
+    if guess not in word_list:
+        guesses_left -= 1
+
