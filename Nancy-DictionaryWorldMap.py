f5bd143b9f412aa200858af30f7cb10f7faf6227
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-02-14 10:47:01 -0800

    2/14/18
    controller

--- Nancy-DictionaryWorldMap.py
+++ Nancy-DictionaryWorldMap.py
@@ -22,3 +22,21 @@ world_map = {
         }
     }
 }
+
+current_node = world_map['WESTHOUSE']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+
+while True:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("You cannot go this way.")
+    else:
+        print('Command not recognized')
