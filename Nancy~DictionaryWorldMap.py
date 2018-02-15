bb858f7c9573a1823ebe7f9569f843fa43a4fdad
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-02-15 10:46:31 -0800

    2/15/18
    rooms

--- Nancy~DictionaryWorldMap.py
+++ Nancy~DictionaryWorldMap.py
@@ -2,24 +2,59 @@ world_map = {
     'STATUE': {
         'NAME': "Tree Statue",
         'DESCRIPTION': "You're standing next to a tree statue, it appears to missing a heart shaped object in its "
-                       "center",
+                       "center, straight ahead is the library",
         'PATHS': {
             'NORTH': 'LIBRARY',
         }
     },
     'LIBRARY': {
         'NAME': "Library",
-        'DESCRIPTION': "You are at the front of the library and the doors are wide open",
+        'DESCRIPTION': "You're inside of the library, there are only two book shelves on each side of the library, "
+                       "straight ahead is combat training room 1",
         'PATHS': {
             'SOUTH': 'STATUE',
-            'NORTH': 'LIBRARY'
+            'NORTH': 'CT1',
+            'WEST': 'SECRET1',
+            'EAST': 'SECRET2'
         }
     },
-    'SOUTHHOUSE': {
-        'NAME': "South of House",
-        'DESCRIPTION': "Insert Description here",
+    'BSE1': {
+        'NAME': "Book shelf",
+        'DESCRIPTION': "You're near the book shelves which are now starting to move, revealing a secret room",
         'PATHS': {
-            'NORTH': 'WESTHOUSE'
+            'WEST': 'SECRET1',
+            'EAST': 'LIBRARY'
+        }
+    },
+    'BSE2': {
+        'NAME': "Book shelves",
+        'DESCRIPTION': "You're near the book shelves which are now starting to move, revealing a secret room",
+        'PATHS': {
+            'EAST': 'SECRET2',
+            'WEST': 'LIBRARY'
+        }
+    },
+    'SECRET1': {
+        'NAME': "Secret room #1",
+        'DESCRIPTION': "You're inside a secret room, on the ground is a big book covered in dust",
+        'PATHS': {
+            'EAST': 'BSE1'
+        }
+    },
+    'SECRET2': {
+        'NAME': "Secret room #2",
+        'DESCRIPTION': "You're inside a secret room, on the ground a ripped page for a special heart object",
+        'PATHS': {
+            'WEST': 'BSE2'
+        }
+    },
+    'CT1': {
+        'NAME': "Secret room #2",
+        'DESCRIPTION': "You're inside a secret room, ",
+        'PATHS': {
+            'EAST': 'SECRET2',
+            'WEST': 'LIBRARY'
+
         }
     }
 }
