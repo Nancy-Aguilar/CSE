f2be0a94ea040c15f70f52b198471e0949e83f91
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-03-20 10:44:57 -0700

    3/20/18
    did nothing again sitll lost and confused, I want and need help but don't have the courage to ask...Welp

--- Items-Nancy.py
+++ Items-Nancy.py
@@ -1,3 +1,7 @@
 class Item(object):
-    def __init__(self, name):
+    def __init__(self, name, durability):
         self.name = name
+        self.durability = durability
+
+class Weapon(Item):
+    def __init__(self, name):
