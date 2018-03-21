02827be3685cbb548d68a2039da638668b2871d3
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-03-21 10:46:03 -0700

    3/21/18
    some work done

--- Items-Nancy.py
+++ Items-Nancy.py
@@ -1,7 +1,23 @@
 class Item(object):
-    def __init__(self, name, durability):
+    def __init__(self, name, description, durability):
         self.name = name
+        self.description = description
         self.durability = durability
+
 
 class Weapon(Item):
     def __init__(self, name):
+        super(Weapon, self).__init__()
+        self.name = name
+
+
+class Sword(Weapon):
+    def __init__(self, name):
+        super(Sword, self).__init__()
+        self.name = name
+
+
+class Bow_Arrow(Weapon):
+    def __init__(self, name):
+        super(Bow_Arrow, self).__init__()
+        self.name = name
