4b8575c6fb6fe0543e809b5122f7e9923eadba57
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-03-22 10:46:46 -0700

    3/22/18
    somewhat of work done but its probably not even right oh well

--- Items-Nancy.py
+++ Items-Nancy.py
@@ -17,7 +17,79 @@ class Sword(Weapon):
         self.name = name
 
 
-class Bow_Arrow(Weapon):
+class BowArrow(Weapon):
     def __init__(self, name):
-        super(Bow_Arrow, self).__init__()
+        super(BowArrow, self).__init__()
+        self.name = name
+
+
+class Keys(Item):
+    def __init__(self, name):
+        super(Keys, self).__init__()
+        self.name = name
+
+
+class CrystalHeart(Keys):
+    def __init__(self, name):
+        super(CrystalHeart, self).__init__()
+        self.name = name
+
+
+class Blackkey(Keys):
+    def __init__(self, name):
+        super(Blackkey, self).__init__()
+        self.name = name
+
+
+class Materials(Item):
+    def __init__(self, name):
+        super(Materials, self).__init__()
+        self.name = name
+
+
+class Carbon(Materials):
+    def __init__(self, name):
+        super(Carbon, self).__init__()
+        self.name = name
+
+
+class Bookofspells(Materials):
+    def __init__(self, name):
+        super(Bookofspells, self).__init__()
+        self.name = name
+
+
+class Potions(Materials):
+    def __init__(self, name):
+        super(Potions, self).__init__()
+        self.name = name
+
+
+class Consumables(Item):
+    def __init__(self, name):
+        super(Consumables, self).__init__()
+        self.name = name
+
+
+class Food(Consumables):
+    def __init__(self, name):
+        super(Food, self).__init__()
+        self.name = name
+
+
+class Medkit(Consumables):
+    def __init__(self, name):
+        super(Medkit, self).__init__()
+        self.name = name
+
+
+class Tools(Item):
+    def __init__(self, name):
+        super(Tools, self).__init__()
+        self.name = name
+
+
+class Flashlight(Tools):
+    def __init__(self, name):
+        super(Flashlight, self).__init__()
         self.name = name
