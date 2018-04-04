9043944727b1acea351699979be8b3f90a92da2e
Nancy-Aguilar <31044677+Nancy-Aguilar@users.noreply.github.com>
2018-04-04 10:45:25 -0700

    4/4/18
    didn't do much still lost

--- Nancy-World Map OOP.py
+++ Nancy-World Map OOP.py
@@ -30,43 +30,43 @@ SECRET2 = Room("Secret Room #2", "You're inside a secret room and the only thing
 CT1 = Room("Combat Training Room #1", "You're inside the training room for 1st years, facing West is the training "
                                       "room for 3rd years facing East is the training room for 2nd years, and North is "
                                       "the training room for 4th years", 'CT4', 'LIBRARY', 'CT2', 'CT3')
-CT2 = Room("Combat Training Room #2", "You are inside the combat training room for 2nd years, on the East side is the "
+CT2 = Room("Combat Training Room #2", "You're inside the combat training room for 2nd years, on the East side is the "
                                       "office on the West side is the combat training room for 1st years", None, None,
            'OFFICE', 'CT1')
-CT3 = Room("Combat Training Room #3", "You are inside the combat training room for 3rd years, on the East side is the "
+CT3 = Room("Combat Training Room #3", "You're inside the combat training room for 3rd years, on the East side is the "
                                       "combat training room for 1st years, on the West side are the dorms for 1st "
                                       "years", None, None, 'CT1', 'DORMS1')
 CT4 = Room("Combat Training Room #4", "You are inside the combat training room for 4th years and up North are the "
                                       "classrooms", 'CLASS', 'CT1', None, None)
 OFFICE = Room("Office", "You are inside the office and facing east is the Nurse's room and facing West is the training "
                         "room for 2nd years", None, None, 'NURSE', 'CT2')
-NURSE = Room("Nurse's Room", "You are inside the Nurse's room and facing east is the Nurse's room and facing North in "
-                             "the lounge room and facing West if the office", 'LOUNGE', None, None, 'OFFICE')
-LOUNGE = Room("Lounge Room", "You're at the lounge room and there's a bag of metals on a sofa, facing West is the "
-                             "cafeteria", None, 'NURSE', None, 'CAFE')
-CAFE = Room("Cafeteria", "You are inside the cafeteria with many tables and chairs and some food trays. Facing East is "
+NURSE = Room("Nurse's Room", "You're inside the Nurse's room and facing east is the Nurse's room and facing North is "
+                             "the lounge room and facing West is the office", 'LOUNGE', None, None, 'OFFICE')
+LOUNGE = Room("Lounge Room", "You're at the lounge room and there is a bag of carbon on a sofa, facing West is the "
+                             "Cafeteria", None, 'NURSE', None, 'CAFE')
+CAFE = Room("Cafeteria", "You are inside the Cafeteria with many tables and chairs and some food trays. Facing East is "
                          "the lounge room", None, None, 'LOUNGE', None)
-DORMS1 = Room("Dorms for 1st years", "You are inside the dorm building, all the rooms are closed and facing North are "
+DORMS1 = Room("Dorms for 1st years", "You are inside a dorm building, all the rooms are closed and facing North are "
                                      "the 2nd year dorms", 'DORMS2', None, 'CT3', None)
 
-DORMS2 = Room("Dorms for 2nd years", "You are inside the dorm building, all the rooms are closed and facing North are "
+DORMS2 = Room("Dorms for 2nd years", "You are inside a dorm building, all the rooms are closed and facing North are "
                                      "the 3rd year dorms", 'DORMS3', 'DORMS1', None, None)
-DORMS3 = Room("Dorms for 3rd years", "You are inside the dorm building, all the rooms are closed and facing North are "
+DORMS3 = Room("Dorms for 3rd years", "You are inside a dorm building, all the rooms are closed and facing North are "
                                      "the 4th year dorms", 'DORMS4', 'DORMS2', None, None)
 
-DORMS4 = Room("Dorms for 4th years", "You are inside the dorm building, all the rooms are closed and facing North is "
+DORMS4 = Room("Dorms for 4th years", "You are inside a dorm building, all the rooms are closed and facing North is "
                                      "the power training room", 'PTR', 'DORMS3', None, None)
 PTR = Room("Power Training Room", "You're in the power training room and there is a station for creating items up "
                                   "North and facing East is the weapon room", 'CREATION', 'DORMS4', 'WEAPON', None)
 CREATION = Room("Creation Table", "You're standing in front of a table that has four potions, you can create anything "
                                   "you want with the right materials, facing East is the weapon room", None, 'DORMS4',
                 'WEAPON', None)
-WEAPON = Room("Weapon Room", "You're inside the weapon room, there are some swords on the ground and there is an "
+WEAPON = Room("Weapon Room", "You're inside the weapon room, there's a sword and bow/arrow on a table and there is an "
                              "entrance to a cave but it's locked and facing South are the classrooms", None, 'CLASS',
               'CP1', 'PTR')
 CLASS = Room("Classroom", "You're inside the classrooms and all the rooms are locked facing North is the weapon room",
              'WEAPON', 'CT4', None, None)
-CP1 = Room("Cave Place #1", "You're inside the cave and people say that there are many dangerous things inside "
+CP1 = Room("Cave Place #1", "You're inside the cave and people say that there are many dangerous things inside and "
                             "multiple places in the cave, there's a light leading South and East", None, 'CP7', 'CP2',
            'WEAPON')
 CP2 = Room("Cave PLace #2", "There is nothing here but a light leading East, South, and West", None, 'CP8', 'CP3',
